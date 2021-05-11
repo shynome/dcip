692fd1c6462068066f461e24783e6a806ccf6455
shynome <shynome@gmail.com>
2021-05-12 02:35:06 +0800

    up version

--- cmd/dcip/dcip.go
+++ cmd/dcip/dcip.go
@@ -32,7 +32,7 @@ func reportError(err error) {
 
 func main() {
 	ctx := kong.Parse(&CLI,
-		kong.Vars{"version": "0.1.0"},
+		kong.Vars{"version": "0.2.0"},
 	)
 	switch ctx.Command() {
 	case "of <[host|name]>":
